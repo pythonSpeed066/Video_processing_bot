77bbd890c923e0d6e7324519dc18b00f794601cb
Daniil Skorokhodov <newds57066@gmail.com>
2023-10-17 00:01:00 +0200

    Сделал пустого бота ;)

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+import os
+
+bot_token = os.getenv("bot_token")
+bot = Bot(token=bot_token)
+dp = Dispatcher(bot)
+
+
+async def on_startup(_):
+    print("Бот запущен!")
+
+
+@dp.message_handler(commands=["start", "help"])
+async def start_command(message: types.Message):
+    await bot.send_message(message.from_id, "Приветствие")
+
+
 if __name__ == '__main__':
-    print("Влад заебал")
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
